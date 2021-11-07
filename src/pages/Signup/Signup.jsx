149d9d3610fb135804d9a26807b8879c8204da4b
therealJimoh <jimoh.abdullah69@gmail.com>
2021-11-07 21:30:30 +0100

    modified signup and signin validation

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -2,49 +2,76 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Signup.css";
 import heroImage from "../../assets/images/Hero-Image.png";
+import Google from "../../assets/images/signup/google.png"
+import Facebook from "../../assets/images/signup/facebook.png"
+import { Form, Formik, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const initialValues = {
+  firstname: "",
+  lastname: "",
+  email: "",
+  password: "",
+};
+
+const validationSchem = Yup.object({
+  firstname: Yup.string().required("Input field is required"),
+  lastname: Yup.string().required("input field is required"),
+  email: Yup.string()
+    .email("Input valid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(8, "Password must be at least 8 characters")
+    .required("Password is required"),
+});
+
+const onSubmit = (values) => {
+  console.log(JSON.stringify(values));
+  
+};
 
 function Signup() {
   return (
-    <section className='userform signup'>
-      <div className='userform__left'>
-        <Link to='/'>
-          <img src={heroImage} alt='TicketXpress' />
+    <section className="userform signup">
+      <div className="userform__left">
+        <Link to="/">
+          <img src={heroImage} alt="TicketXpress" />
         </Link>
       </div>
-      <div className='userform__right'>
-        <div className='useform__conn'>
+      <div className="userform__right">
+        <div className="useform__conn">
           <h1>Get Started</h1>
-          <p className='userform__switch'>
+          <p className="userform__switch">
             Already Have An Account?
-            <Link to='/signin'>Login</Link>
+            <Link to="/signin">Login</Link>
           </p>
 
-          <div className='alternative__signin'>
-            <div className='alt alt1'>
+          <div className="alternative__signin">
+            <div className="alt alt1">
               <button>
                 <Link
-                  to='https://www.freepnglogos.com/pics/google-logo'
-                  title='Image from freepnglogos.com'
+                  to="https://google.com"
+                  title="google"
                 >
                   <img
-                    src='https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png'
-                    width='200'
-                    alt='google logo png webinar optimizing for success google business webinar'
+                    src={Google}
+                    width="200"
+                    alt="google logo png webinar optimizing for success google business webinar"
                   />
                 </Link>
                 Sign Up With Google
               </button>
             </div>
-            <div className='alt alt2'>
+            <div className="alt alt2">
               <button>
                 <Link
-                  to='https://www.freepnglogos.com/pics/facebook-logo'
-                  title='Image from freepnglogos.com'
+                  to="https://facebook.com"
+                  title="facebook"
                 >
                   <img
-                    src='https://www.freepnglogos.com/uploads/facebook-logo-13.png'
-                    width='200'
-                    alt='logo facebook download png'
+                    src={Facebook}
+                    width="200"
+                    alt="facebook"
                   />
                 </Link>
                 Sign Up With Facebook
@@ -52,70 +79,78 @@ function Signup() {
             </div>
           </div>
 
-          <div className='or'>
+          <div className="or">
             <hr />
             <p>OR</p>
             <hr />
           </div>
 
-          <p id='authError' className='error'></p>
+          <p id="authError" className="error"></p>
 
-          <form action='' className='userformed'>
-            <div className='row'>
-              <div className='formGroup'>
-                <label for='firstname'>First Name</label>
-                <input
-                  id='firstNameField'
-                  type='text'
-                  required
-                  pattern='(.*)\s(.*)'
-                  className='form-control'
-                />
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchem}
+            onSubmit={onSubmit}
+          >
+            <Form>
+              <div className="row">
+                <div className="formGroup">
+                  <label for="firstname">First Name</label>
+                  <Field
+                    id="firstNameField"
+                    type="text"
+                    name="firstname"
+                    className="form-control"
+                  />
+                  <ErrorMessage name="firstname" component="div" />
+                </div>
+                <div className="formGroup">
+                  <label for="lastname">Last Name</label>
+                  <Field
+                    id="lastNameField"
+                    type="text"
+                    name="lastname"
+                    className="form-control"
+                  />
+                  <ErrorMessage name="lastname" component="div" />
+                </div>
               </div>
-              <div className='formGroup'>
-                <label for='lastname'>Last Name</label>
-                <input
-                  id='lastNameField'
-                  type='text'
-                  required
-                  pattern='(.*)\s(.*)'
-                  className='form-control'
+              <div className="formGroup">
+                <label for="emailaddress">Email Address</label>
+                <Field
+                  id="emailField"
+                  type="email"
+                  name="email"
+                  className="form-control"
                 />
+                <ErrorMessage name="email" component="div" />
               </div>
-            </div>
-            <div className='formGroup'>
-              <label for='emailaddress'>Email Address</label>
-              <input
-                id='emailField'
-                type='email'
-                required
-                className='form-control'
-              />
-            </div>
-            <div className='formGroup'>
-              <label for='lastname'>Password</label>
-              <input
-                id='passwordField'
-                type='password'
-                required
-                className='form-control'
-              />
-            </div>
+              <div className="formGroup">
+                <label for="password">Password</label>
+                <Field
+                  id="passwordField"
+                  type="password"
+                  name="password"
+                  className="form-control"
+                />
+                <ErrorMessage name="password" component="password" />
+              </div>
 
-            <div className='condition'>
-              <input type='checkbox' name='' id='' />
-              <label for='condition'>
-                I agree to the platform's
-                <a href='/'>Terms Of Services</a>
-                and
-                <a href='/'>Privacy And Policy</a>
-              </label>
-            </div>
+              <div className="condition">
+                <input type="checkbox" name="" id="" />
+                <label for="condition">
+                  I agree to the platform's
+                  <a href="/">Terms Of Services</a>
+                  and
+                  <a href="/">Privacy And Policy</a>
+                </label>
+              </div>
 
-            <div className='signsub'>
-              <input id='signUpButton' type='submit' value='Register' />
-            </div>
-          </form>
+              <div className="signsub">
+                <input id="signUpButton" type="submit" value="Register" />
+              </div>
+            </Form>
+          </Formik>
         </div>
       </div>
     </section>
