9c57c0b4b88ffa4eed420cfc0a028617461158fd
Harriet-code <harriosas@gmail.com>
2021-11-10 00:35:38 +0100

    first commit

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,76 +4,76 @@ import goodByeImage from "../../assets/images/travel 1.png";
 
 function Contact() {
   return (
-    <section className='contact__page'>
-      <div className='contact-left'>
-        <div className='contact-text'>
+    <section className="contact__page">
+      <div className="contact-left">
+        <div className="contact-text">
           <h1>We’d love to</h1>
           <h1>hear from You!</h1>
         </div>
-        <div className='phone'>
-          <i className='fas fa-phone-alt' style={{ fontSize: "20px" }}></i>
+        <div className="phone">
+          <i className="fas fa-phone-alt" style={{ fontSize: "20px" }}></i>
           +234 123 4567
         </div>
-        <div className='msg'>
-          <i className='far fa-envelope' style={{ fontSize: "20px" }}></i>
+        <div className="msg">
+          <i className="far fa-envelope" style={{ fontSize: "20px" }}></i>
           info@ticketxpress.com
         </div>
 
-        <div className='contact-img'>
-          <img src={goodByeImage} alt='img' />
+        <div className="contact-img">
+          <img src={goodByeImage} alt="img" />
         </div>
       </div>
 
-      <div className='contact-right'>
+      <div className="contact-right">
         <h1>Contact Us</h1>
-        <form className='contactform'>
-          <div className='formGroup'>
-            <label for='name'>Full Name</label>
+        <form className="contactform">
+          <div className="formGroup">
+            <label for="name">Full Name</label>
             <input
-              id='name'
-              type='text'
-              placeholder='Full Name'
+              id="name"
+              type="text"
+              placeholder="Full Name"
               required
-              className='form-control'
+              className="form-control"
             />
-            <div className='error-hint hidden'>Your name is required</div>
+            <div className="error-hint hidden">Your name is required</div>
           </div>
-          <div className='row'>
-            <div className='formGroup'>
-              <label for='email'>Email</label>
+          <div className="row">
+            <div className="formGroup">
+              <label for="email">Email</label>
               <input
-                id='email'
-                type='email'
+                id="email"
+                type="email"
                 required
-                className='form-control'
-                placeholder='Enter Email'
+                className="form-control"
+                placeholder="Enter Email"
               />
-              <div className='error-hint hidden'>Your Email is required</div>
+              <div className="error-hint hidden">Your Email is required</div>
             </div>
-            <div className='formGroup'>
-              <label for='lastname'>Phone Number</label>
+            <div className="formGroup">
+              <label for="lastname">Phone Number</label>
               <input
-                id='phone'
-                type='text'
+                id="phone"
+                type="text"
                 required
-                placeholder='Enter Phone number'
-                className='form-control'
+                placeholder="Enter Phone number"
+                className="form-control"
               />
             </div>
           </div>
 
-          <div className='formGroup'>
-            <label for='text'>Message</label>
-            <textarea id='message' placeholder='Message Here'>
+          <div className="formGroup">
+            <label for="text">Message</label>
+            <textarea id="message" placeholder="Message Here">
               {" "}
             </textarea>
           </div>
 
-          <div className='submitBtn'>
-            <button className='subBtn'>Submit</button>
+          <div className="submitBtn">
+            <button className="subBtn">Submit</button>
           </div>
         </form>
-        <div className='hidden thank-you'>Thank you for your feedback</div>
+        <div className="hidden thank-you">Thank you for your feedback</div>
       </div>
     </section>
   );
--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -5,46 +5,46 @@ import heroImage from "../../assets/images/Hero-Image.png";
 
 function Signin() {
   return (
-    <section className='userform signup'>
-      <div className='userform__left'>
-        <Link to='/'>
-          <img src={heroImage} alt='TicketXpress' />
+    <section className="userform signup">
+      <div className="userform__left">
+        <Link to="/">
+          <img src={heroImage} alt="TicketXpress" />
         </Link>
       </div>
-      <div className='userform__right'>
-        <div className='useform__conn'>
+      <div className="userform__right">
+        <div className="useform__conn">
           <h1>Welcome</h1>
-          <p className='userform__switch'>
+          <p className="userform__switch">
             Don't Have An Account?
-            <Link to='/signup'>Sign Up</Link>
+            <Link to="/signup">Sign Up</Link>
           </p>
 
-          <div className='alternative__signin'>
-            <div className='alt alt1'>
+          <div className="alternative__signin">
+            <div className="alt alt1">
               <button>
                 <a
-                  href='https://www.freepnglogos.com/pics/google-logo'
-                  title='Image from freepnglogos.com'
+                  href="https://www.freepnglogos.com/pics/google-logo"
+                  title="Image from freepnglogos.com"
                 >
                   <img
-                    src='https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png'
-                    width='200'
-                    alt='google logo png webinar optimizing for success google business webinar'
+                    src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
+                    width="200"
+                    alt="google logo png webinar optimizing for success google business webinar"
                   />
                 </a>
                 Sign Up With Google
               </button>
             </div>
-            <div className='alt alt2'>
+            <div className="alt alt2">
               <button>
                 <Link
-                  to='https://www.freepnglogos.com/pics/facebook-logo'
-                  title='Image from freepnglogos.com'
+                  to="https://www.freepnglogos.com/pics/facebook-logo"
+                  title="Image from freepnglogos.com"
                 >
                   <img
-                    src='https://www.freepnglogos.com/uploads/facebook-logo-13.png'
-                    width='200'
-                    alt='logo facebook download png'
+                    src="https://www.freepnglogos.com/uploads/facebook-logo-13.png"
+                    width="200"
+                    alt="logo facebook download png"
                   />
                 </Link>
                 Sign Up With Facebook
@@ -52,37 +52,37 @@ function Signin() {
             </div>
           </div>
 
-          <div className='or'>
+          <div className="or">
             <hr />
             <p>OR</p>
             <hr />
           </div>
 
-          <p id='authError' className='error'></p>
+          <p id="authError" className="error"></p>
 
-          <form action='' className='userformed'>
-            <div className='formGroup'>
-              <label for='emailaddress'>Email Address</label>
-              <input id='emailField' type='email' />
+          <form action="" className="userformed">
+            <div className="formGroup">
+              <label for="emailaddress">Email Address</label>
+              <input id="emailField" type="email" />
             </div>
-            <div className='formGroup'>
-              <label for='lastname'>Password</label>
-              <input id='passwordField' type='password' />
+            <div className="formGroup">
+              <label for="lastname">Password</label>
+              <input id="passwordField" type="password" />
             </div>
 
-            <div className='remember'>
-              <div className='rememberMe'>
-                <input type='checkbox' name='' id='' />
-                <label for=''>Remeber Me</label>
+            <div className="remember">
+              <div className="rememberMe">
+                <input type="checkbox" name="" id="" />
+                <label for="">Remember Me</label>
               </div>
 
-              <div className='forgotPass'>
-                <a href='/'>Forgot Password?</a>
+              <div className="forgotPass">
+                <a href="/">Forgot Password?</a>
               </div>
             </div>
 
-            <div className='signsub'>
-              <input id='loginButton' type='submit' value='Login' />
+            <div className="signsub">
+              <input id="loginButton" type="submit" value="Login" />
             </div>
           </form>
         </div>
