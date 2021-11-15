85c5169ecc7a34b90610712171b50d537f09e456
EdwardGold Edem Hogan <edge37@outlook.com>
2021-11-15 14:39:54 +0100

    ha

--- src/pages/FlightPage/FlightPage.jsx
+++ src/pages/FlightPage/FlightPage.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import FlightTicket from "../../components/FlightTicket";
 import Services from "../../components/Services";
 import "./FlightPage.css";
-
 function FlightPage() {
+  let [depDate, setDepDate] = useState('')
+  let [origin, setOrigin] = useState('')
+  let [destination, setDestination] = useState('')
+
   return (
     <div>
-      <FlightTicket />
+      <FlightTicket
+        setDepDate={setDepDate}
+        setOrigin={setOrigin}
+        setDestination={setDestination}
+      />
       <Services />
     </div>
   );
