0668775a644fac3ae8b11ae6c398cc4de357d5f6
Alabi Abayomi <abayomi15@gmail.com>
2021-11-16 12:35:22 +0100

    added icons

--- src/components/NotificationBar/NotificationBar.jsx
+++ src/components/NotificationBar/NotificationBar.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import Clock from "../../assets/images/Notificationbar/clock.png";
 import "./NotificationBar.css";
 
 function NotificationBar() {
   return (
     <div>
-    <div className="notification">
-      <img src={Clock} alt="Clock" className="clock" />
-      <p className="notification_text">Nice Job! you picked one of the best value flight. <strong>Book now, it only takes a few minutes.</strong></p>
-    </div>
+      <div className="notification">
+        <i className="far fa-3x fa-clock"></i>
+        <p className="notification_text">
+          Nice Job! you picked one of the best value flight.{" "}
+          <strong>Book now, it only takes a few minutes.</strong>
+        </p>
+      </div>
       <div className="notification-_header">
         <p>Customize your trip</p>
-    </div>
+      </div>
     </div>
   );
 }
--- src/pages/CustomerInformation/CustomerInformation.jsx
+++ src/pages/CustomerInformation/CustomerInformation.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import useTitle from "../../hooks/useTItle/useTitle";
+import Brand from "../../assets/images/Customerinformation/expedia.png"
+import { Link } from "react-router-dom";
 import NotificationBar from '../../components/NotificationBar';
 import "./CustomerInformation.css"
 
@@ -10,10 +12,121 @@ function CustomerInformation() {
   return (
     <main>
       <NotificationBar />
-      <div className="info">
-        <div className="flight_itinerary"> I am here</div>
-        <div className= "customer_information"> I am here</div>
-        <div className="confirmation"> I am here</div>
+      <div className="whole">
+        <div className="sidebar">Sidebar</div>
+        <div className="main">
+          <div className="info">
+            <div className="flight_itinerary">
+              <div className="flight_itinerary_header">
+                <p>Flight Itinerary</p>
+                <Link to="/">Change</Link>
+              </div>
+              <hr />
+              <div className="flight_itinerary_body">
+                <div className="brand">
+                  <img src={Brand} alt="brand" />
+                </div>
+                <div className="flight_itinerary_body_brand_info">
+                  <p>Expedia</p>
+                  <h3>Oct 18, 2021</h3>
+                </div>
+              </div>
+              <div className="flight_itinerary_info">
+                <div className="flight_itinerary_info_location">
+                  <p>
+                    <strong>17:00</strong>
+                    (ABV)
+                  </p>
+                  <p>Abuja.</p>
+                  <p>
+                    <small>Nnamdi Azikwe International Airport</small>
+                  </p>
+                </div>
+                <div className="flight_itinerary_info_dnt">
+                  <p>
+                    <strong>0h 0m</strong>
+                  </p>
+                  <hr />
+                  <p>
+                    <strong>0 stops</strong>
+                  </p>
+                </div>
+                <div className="flight_itinerary_info_location">
+                  <p>
+                    <strong>18:00</strong>
+                    (LOS)
+                  </p>
+                  <p>Lagos.</p>
+                  <p>
+                    <small>Muritala Muhammed International Airport</small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="info">
+            <div className="flight_itinerary">
+              <div className="flight_itinerary_header">
+                <p>Who is Traveling?</p>
+                <Link to="/">
+                  Your personal data is protected.
+                  <i class="fas fa-lock"></i>
+                </Link>
+              </div>
+              <hr />
+              <div className="flight_itinerary_body">
+                <div className="brand">
+                  <img src={Brand} alt="brand" />
+                </div>
+                <div className="flight_itinerary_body_brand_info">
+                  <p>Expedia</p>
+                  <h3>Oct 18, 2021</h3>
+                </div>
+              </div>
+              <div className="flight_itinerary_info">
+                <div className="flight_itinerary_info_location">
+                  <p>
+                    <strong>17:00</strong>
+                    (ABV)
+                  </p>
+                  <p>Abuja.</p>
+                  <p>
+                    <small>Nnamdi Azikwe International Airport</small>
+                  </p>
+                </div>
+                <div className="flight_itinerary_info_dnt">
+                  <p>
+                    <strong>0h 0m</strong>
+                  </p>
+                  <hr />
+                  <p>
+                    <strong>0 stops</strong>
+                  </p>
+                </div>
+                <div className="flight_itinerary_info_location">
+                  <p>
+                    <strong>18:00</strong>
+                    (LOS)
+                  </p>
+                  <p>Lagos.</p>
+                  <p>
+                    <small>Muritala Muhammed International Airport</small>
+                  </p>
+                </div>
+              </div>
+              <div className="blue_notice">
+                <i className="fas fa-3x fa-info-circle"></i>
+                <p>
+                  Use all given Names and surname exactly as they appear on the
+                  passport/ID to avoid conflict actions.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="info">Here</div>
+          <div className="info">Here</div>
+          <div className="info">Here</div>
+        </div>
       </div>
     </main>
   );
