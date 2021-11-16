3e8fa9ada66be0be7c67df4334b335bdd8282c24
EdwardGold Edem Hogan <edge37@outlook.com>
2021-11-16 10:17:08 +0100

    c

--- src/utils/className.js
+++ src/utils/className.js
@@ -1,31 +1,53 @@
-let args = process.argv.slice(2)
+let args = process.argv.slice(2);
 
 const fs = require("fs");
+const path = require("path");
 
-let module_name = args[2];
 let basePath = args[0];
+let module_name = args[1];
+let exclude = args[2];
 
-let classNameRegex = /(?:class|className)=['|"](.+)['|"]/g;
-let classRegex = /(\S+)/g;
+let excludeRegEx = new RegExp(`^${exclude}`)
+
+let classNameRegEx = /(?:class|className)=['|"](.+)['|"]/g;
+let classRegEx = /(\S+)/g;
 
 function classReplace(match, _class) {
   return `$\{${module_name}.${_class}}`;
 }
 
 function classNameReplace(match, classList) {
-  let classes = classList.replace(classRegex, classReplace);
+  let classes = classList.replace(classRegEx, classReplace);
   return `className={\`${classes}\`}`;
 }
 
 function replaceCss(content) {
-  return content.replace(classNameRegex, classNameReplace);
+  return content.replace(classNameRegEx, classNameReplace);
 }
 
-function CssFileReplace(path) {
-    let content = fs.readFilesync(path)
-    let replaced = replaceCss(content)
-    fs.writeFileSync(path, replaced)
+function CssFileReplace(_path) {
+  let content = fs.readFileSync(_path, 'utf8');
+  let replaced = replaceCss(content);
+  fs.writeFileSync(_path, replaced);
 }
 
-let paths = fs.readdirSync(basePath)
-paths.forEach(path => CssFileReplace(path))
+const replace = (base) => {
+  fs.readdir(base, { withFileTypes: true }, (err, dirents) => {
+    dirents.forEach((dirent) => {
+      let fullPath = path.join(base, dirent.name);
+      console.log(fullPath)
+      if (dirent.isDirectory()) {
+        replace(fullPath)
+        return
+      }
+      if (
+        path.extname(dirent.name) === (".jsx" || ".js")
+        && !(excludeRegEx.test(dirent.name))
+        ) {
+        CssFileReplace(fullPath)
+      }
+    });
+  });
+};
+
+replace(basePath);
